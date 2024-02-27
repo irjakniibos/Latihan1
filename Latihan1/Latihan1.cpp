fe01e0fd8dafaf80de089f5dd73e58b5338be367
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-02-27 13:52:35 +0700

    Add project files.

---
+++ Latihan1/Latihan1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{//begin
+	//pseudocode
+	// numeric nAge
+	// dipslay 'umur saya adalah' + nUmur 'salam kenal ya'
+	
+	int nUmur = 35;
+	cout << "umur saya adalah " << nUmur << endl << "Salam kenal ya";
+	
+
+
+}//end
+
+
